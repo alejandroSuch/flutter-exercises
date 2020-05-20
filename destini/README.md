b6a6de23145b33bd53fc588171f58dec7c96e819
Alejandro Such <alejandro.such@gmail.com>
2020-05-20 17:16:26 +0200

    All steps done

--- destini/README.md
+++ destini/README.md
@@ -3,3 +3,4 @@
 
 # Destini 🤔
 
+![](https://user-images.githubusercontent.com/53031/82463765-82ab9200-9abd-11ea-92e2-09eab514b181.png)
--- destini/lib/story.dart
+++ destini/lib/story.dart
@@ -2,6 +2,8 @@ class Story {
   String storyTitle;
   String choice1;
   String choice2;
+  int choice1GoesTo;
+  int choice2GoesTo;
 
-  Story({this.storyTitle, this.choice1, this.choice2});
+  Story({this.storyTitle, this.choice1, this.choice2, this.choice1GoesTo, this.choice2GoesTo});
 }
